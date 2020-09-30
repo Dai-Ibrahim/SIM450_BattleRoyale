5d1c03080263d8c532e61ee3441afb61afb94b7f
Dai Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2020-09-29 23:45:26 -0700

    Add files via upload

---
+++ Script/Swing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swing : MonoBehaviour
+{
+	public float rotFl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(gameObject.tag == "x")
+		{
+			 transform.localEulerAngles = new Vector3(-Mathf.PingPong(Time.time*40, rotFl), 0, 0); 
+
+		}
+		
+		else if(gameObject.tag == "y")
+		{
+		 	transform.localEulerAngles = new Vector3(0, 0,-Mathf.PingPong(Time.time*40, rotFl)); 
+
+		}
+		 
+
+        
+    }
+}
